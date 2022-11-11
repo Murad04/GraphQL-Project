f2cfe4dc01b313c57c02d42a841b9a8764e94bf4
Murad <67361462+Murad04@users.noreply.github.com>
2022-11-11 22:32:15 +0400

    Added AddCommandToDB migration

--- GraphQL/Data/AppDBContext.cs
+++ GraphQL/Data/AppDBContext.cs
@@ -10,5 +10,22 @@ namespace GraphQL.Data
         }
 
         public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Platform>()
+                .HasMany(p => p.Commands)
+                .WithOne(p => p.Platform!)
+                .HasForeignKey(p => p.PlatformID);
+
+            modelBuilder.Entity<Command>()
+                .HasOne(c => c.Platform)
+                .WithMany(c => c.Commands)
+                .HasForeignKey(p => p.PlatformID);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
---
+++ GraphQL/Migrations/20221111183113_AddCommandtoDB.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCommandtoDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HowTo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommandLine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PlatformID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commands_Platforms_PlatformID",
+                        column: x => x.PlatformID,
+                        principalTable: "Platforms",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commands_PlatformID",
+                table: "Commands",
+                column: "PlatformID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
